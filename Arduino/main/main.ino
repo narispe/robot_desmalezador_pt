864b05eff5076212f75e69e875ffe43a676f7ffb
Néstor Arispe Salinas <narispe@uc.cl>
2025-06-17 09:24:13 -0400

    add main

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -6,16 +6,12 @@
 #define SERIAL 115200
 // BT
 #define MAC "90:34:fc:0f:69:75"
-// LIMIT SWITCH
-#define LS_X 32
-#define LS_Y 33
-#define LS_Z 25
 // STEPPER X
 #define STEP_X 23
 #define DIR_X 22
 #define PLUS_X 1
 #define MINUS_X 0
-#define X_PER_STEP 1.8  //°
+#define X_PER_STEP 0.1286 //°
 // STEPPER Y
 #define STEP_Y 19
 #define DIR_Y 18
@@ -29,10 +25,14 @@
 #define MINUS_Z 0
 #define Z_PER_STEP 0.015 //mm
 // STEPPER
-#define T_INIT 2000  //ms
-#define T_STEP 1000  //us
-#define T_DIR 200  //ms
+#define T_INIT 2000 //ms
+#define T_STEP 1000 //us
+#define T_DIR 200   //ms
 #define STEP_PER_REV 200
+// LIMIT SWITCH
+#define LS_X 32
+#define LS_Y 33
+#define LS_Z 25
 // PUENTE H
 #define ENA 14
 #define IN1 27
@@ -62,31 +62,20 @@ int pwm_val;
 int rpm_values[3] = {0, 200, 400};
 int rpm_val_index;
 int rpm_ref = rpm_values[0];
-float STATE[3];
+float STATE[3] = {0.0, 0.0, 0.0};
 
 
 //---------SETUP-----------
 void setup() {
   Serial.begin(SERIAL);
   // cnc shield
-  for (int pin : {STEP_X, DIR_X, STEP_Y, DIR_Y, STEP_Z, DIR_Z}){
+  for ( int pin : {STEP_X, DIR_X, STEP_Y, DIR_Y, STEP_Z, DIR_Z} ){
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
   }
   digitalWrite(DIR_X, PLUS_X);
   digitalWrite(DIR_Y, PLUS_Y);
   digitalWrite(DIR_Z, PLUS_Z);
-  // puente h
-  ledcAttach(ENA, PWM_FREQ, PWM_RES);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  // encoder
-  pinMode(V3, OUTPUT);
-  digitalWrite(V3, HIGH);
-  pinMode(ENCA, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ENCA), ISR_Encoder1, CHANGE);
-  pinMode(ENCB, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ENCB), ISR_Encoder2, CHANGE);
   // limit switch
   pinMode(LS_X, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(LS_X), ISR_LimitSwitchX, CHANGE);
@@ -94,8 +83,21 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(LS_Y), ISR_LimitSwitchY, CHANGE);
   pinMode(LS_Z, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(LS_Z), ISR_LimitSwitchZ, CHANGE);
+  // puente h
+  ledcAttach(ENA, PWM_FREQ, PWM_RES);
+  for ( int pin : {IN1, IN2} ){
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+  }
+  // encoder
+  pinMode(V3, OUTPUT);
+  digitalWrite(V3, HIGH);
+  pinMode(ENCA, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCA), ISR_Encoder1, CHANGE);
+  pinMode(ENCB, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCB), ISR_Encoder2, CHANGE);
   // bt
-  Ps3.attach(notify);
+  Ps3.attach(ISR_Ps3);
   Ps3.begin(MAC);
   t_prev = millis();
 }
@@ -104,7 +106,30 @@ void setup() {
 //---------LOOP-----------
 void loop() {
   // Rotate while pressing
-  delay(1000);
+  if ( Ps3.data.button.left || Ps3.data.button.right )
+    step_pulse(STEP_X);
+  else if ( Ps3.data.button.down || Ps3.data.button.up )
+    step_pulse(STEP_Y);
+  else if ( Ps3.data.button.l2 || Ps3.data.button.r2 )
+    step_pulse(STEP_Z);
+
+  t = millis();
+  if ( t - t_prev >= T_S ){
+    noInterrupts();
+    theta = encoder_count;
+    interrupts();
+    dt = t - t_prev;
+    rpm = (theta - theta_prev) * (60000/dt) / NFactor;
+    e = rpm_ref - rpm;
+    inte += (e + e_prev) * (dt/2000);
+    deriv = (e - e_prev) * (1000/dt);
+    pwm_val = int(KP*e + KI*inte + KD*deriv);
+    pwm_val = constrain(pwm_val, -255, 255);
+    WriteDriver(pwm_val);
+    theta_prev = theta;
+    e_prev = e;
+    t_prev = t;
+  }
 
 }
 
@@ -131,7 +156,7 @@ void change_dir(int dir_pin, bool dir_val){
   }
 }
 // BT
-void notify(){
+void ISR_Ps3(){
   // Change dir only when pressing opposite
   if ( Ps3.event.button_down.up )
     change_dir(DIR_Y, PLUS_Y);
@@ -170,15 +195,15 @@ void notify(){
     change_dir(DIR_Z, PLUS_Z);
     step_pulse(STEP_Z);
   }
-
-  else if (Ps3.event.button_down.start) {
+  // pid control of rpm
+  else if ( Ps3.event.button_down.start ) {
     rpm_val_index = (rpm_val_index + 1) % 3;
     rpm_ref = rpm_values[rpm_val_index];
     inte = 0;
   }
-  else if (Ps3.event.button_down.select) {
+  else if ( Ps3.event.button_down.select ) {
     rpm_val_index = 0;
-    rpm_ref = rpm_values[0];
+    rpm_ref = rpm_values[rpm_val_index];
     inte = 0;
   }
 }
@@ -204,7 +229,6 @@ void WriteDriver(int PWM_val){
   }
   ledcWrite(ENA, duty);
 }
-// LIMIT SWITCH
 void ISR_LimitSwitchX(){
   if (digitalRead(LS_X) == LOW){
     Serial.println("LIMIT SWITCH X ACTIVADO");
